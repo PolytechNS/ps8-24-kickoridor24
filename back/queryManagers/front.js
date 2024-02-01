d71cc203c44200412a1f3a02be15ebb4a445cc55
julienvassard <julien.vassard@etu.univ-cotedazur.fr>
2024-02-01 13:38:24 +0100

    add game

--- back/queryManagers/front.js
+++ back/queryManagers/front.js
@@ -39,6 +39,7 @@ function manageRequest(request, response) {
     const parsedUrl = url.parse(baseFrontPath + request.url);
     let pathName = `.${parsedUrl.pathname}`;
     let extension = path.parse(pathName).ext;
+
     // Uncomment the line below if you want to check in the console what url.parse() and path.parse() create.
     //console.log(parsedUrl, pathName, path.parse(pathName));
 
