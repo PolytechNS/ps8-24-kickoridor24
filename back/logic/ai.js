410ad2373ad1f60b2b20f08121a4e8b4a0d1e4e5
romainPellegrini <romain.pellegrini@etu.unice.fr>
2024-02-04 11:54:43 +0100

    ajout condition méthode bot

--- back/logic/ai.js
+++ back/logic/ai.js
@@ -4,13 +4,21 @@ function computeMove(gameState) {
     let possibleMoves = [];
     // Check if moving left is possible.
     var validMoves = getValidMoves(pos);
-    if ((pos%17) >= 2) possibleMoves.push(pos-2);
+    if ((pos%17) >= 2)
+        if(validMoves.includes(pos-2))
+            possibleMoves.push(pos-2);
     // Check if moving right is possible.
-    if ((pos%17)!= 16) possibleMoves.push(pos+2);
+    if ((pos%17)!= 16)
+        if(validMoves.includes(pos+2))
+            possibleMoves.push(pos+2);
     // Check if moving down is possible.
-    if (pos < 255) possibleMoves.push(pos+ 34);
+    if (pos < 255)
+        if(validMoves.includes(pos+34))
+        possibleMoves.push(pos+ 34);
     // Check if moving up is possible.
-    if (pos > 16) possibleMoves.push(pos-34);
+    if (pos > 16)
+        if(validMoves.includes(pos-34))
+        possibleMoves.push(pos-34);
 
     // Get a random integer between 0 and possibleMoves.length-1
     let moveIndex = Math.floor(Math.random()*possibleMoves.length);
