f73ccf17ad84a2d0d269a9852146e9708d66007e
Ziad-Bouhlel <ziad.bouhlel@gmail.com>
2024-02-05 10:58:20 +0100

    setup login implementation

--- back/index.js
+++ back/index.js
@@ -7,6 +7,35 @@ const apiQuery = require('./queryManagers/api.js')
 /* The http module contains a createServer function, which takes one argument, which is the function that
 ** will be called whenever a new request arrives to the server.
  */
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+
+const uri = 'mongodb://localhost:27017';
+
+const client = new MongoClient(uri,  {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    }
+);
+
+async function run() {
+    try {
+        // Connect the client to the server (optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
 const server = http.createServer(function (request, response) {
     // First, let's check the URL to see if it's a REST request or a file request.
     // We will remove all cases of "../" in the url for security purposes.
@@ -52,3 +81,4 @@ const io = new Server(server);
 server.listen(8000, ()=> {
     console.log('Server is listening on port 8000');
 });
+
