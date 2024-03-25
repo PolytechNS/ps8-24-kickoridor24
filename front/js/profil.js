c94827d39c4cf319e2eb2a2b68907fc37f6b467c
RomainPellegrini <romain.pellegrini@etu.unice.fr>
2024-03-25 23:59:30 +0100

    css du profil

--- front/js/profil.js
+++ front/js/profil.js
@@ -1,3 +1,8 @@
+const photos = ['Bellingham','Haaland','Harit','Kane','Lacazette','Mbappe','Mitroglu'
+    ,'Modric','Pedri','Salah','Son','Vlahovic'];
+
+const celebrations = ['Belli-goal','Charo','Dab','SIUU'];
+
 function showCeleb(){
     document.getElementsByClassName("info")[0].style.display = "none";
     document.getElementsByClassName("photo")[0].style.display = "none";
@@ -8,6 +13,8 @@ function showCeleb(){
     document.getElementById("celeb").style.backgroundColor = "#3EE4F0";
     document.getElementById("info").style.backgroundColor = "#E4E5E7";
     document.getElementById("photo").style.backgroundColor = "#E4E5E7";
+    fillCelebration();
+
 }
 
 function showInfo(){
@@ -32,4 +39,84 @@ function showPhoto(){
     document.getElementById("celeb").style.backgroundColor = "#E4E5E7";
     document.getElementById("info").style.backgroundColor = "#E4E5E7";
     document.getElementById("photo").style.backgroundColor = "#3EE4F0";
+    fillPhoto();
+}
+showCeleb();
+
+var gif = document.getElementById('gifResume');
+
+gif.addEventListener('mouseenter', function() {
+    var tmp = gif.src;
+    tmp = tmp.replace("png","gif");
+    gif.src = tmp;
+});
+
+gif.addEventListener('mouseleave', function() {
+    var tmp = gif.src;
+    tmp = tmp.replace("gif","png");
+    gif.src = tmp;
+});
+
+function fillPhoto(){
+    var imagesContainer = document.getElementById('gridpdp');
+    while (imagesContainer.firstChild){
+        imagesContainer.removeChild(imagesContainer.firstChild);
+    }
+    photos.forEach(fileName => {
+        const div = document.createElement('div');
+        div.classList.add("affichagePhoto");
+        const img = document.createElement('img');
+
+        img.src = '../images/photoProfil/'+fileName+'.png';
+      
+        div.appendChild(img);
+        const p = document.createElement('p');
+        p.textContent = fileName;
+        div.appendChild(p);
+        imagesContainer.appendChild(div);
+    });
+}
+
+function fillCelebration(){
+    var celebChoisie = document.getElementById('celebrationChoisieCeleb');
+
+    celebChoisie.addEventListener('mouseenter', function() {
+        var tmp = celebChoisie.src;
+        tmp = tmp.replace("png","gif");
+        celebChoisie.src = tmp;
+    });
+
+    celebChoisie.addEventListener('mouseleave', function() {
+        var tmp = celebChoisie.src;
+        tmp = tmp.replace("gif","png");
+        celebChoisie.src = tmp;
+    });
+
+    var imagesContainer = document.getElementById('gridCeleb');
+    while (imagesContainer.firstChild){
+        imagesContainer.removeChild(imagesContainer.firstChild);
+    }
+    celebrations.forEach(fileName => {
+        const div = document.createElement('div');
+        div.classList.add("affichageCeleb");
+        const img = document.createElement('img');
+
+        img.src = '../images/celebration/'+fileName+'.png';
+        img.addEventListener('mouseenter', function() {
+            var tmp = img.src;
+            tmp = tmp.replace("png","gif");
+            img.src = tmp;
+        });
+
+        img.addEventListener('mouseleave', function() {
+            var tmp = img.src;
+            tmp = tmp.replace("gif","png");
+            img.src = tmp;
+        });
+        div.appendChild(img);
+        const p = document.createElement('p');
+        p.textContent = fileName;
+        div.appendChild(p);
+        imagesContainer.appendChild(div);
+    });
 }
